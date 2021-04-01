97474308dabe7e0c79d398ca7f44762e3a67e7f1
Lucasmarietan <lucas.marietan@he-arc.ch>
2021-04-01 11:39:34 +0200

    Ajout des relations d'amitié

---
+++ src/ch/hearc/business/Cook.java
@@ -0,0 +1,9 @@
+package ch.hearc.business;
+
+// Arc pour la cuisine
+public class Cook extends Edge {
+
+    public Cook(String name, Receipt ReceiptDest, double metric){
+        super(name, ReceiptDest, metric);
+    }
+}
---
+++ src/ch/hearc/business/Receipt.java
@@ -0,0 +1,15 @@
+package ch.hearc.business;
+
+// Noeud pour la cuisine
+public class Receipt extends Node {
+    private String name;
+
+    /**
+     * Constructeurs
+     */
+
+    public Receipt(String name){
+        super(name);
+    }
+
+}
